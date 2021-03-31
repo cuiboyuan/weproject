02803ec18f1708053b41fd2c78414ba368d9a295
Boyuan Cui <boyuanbryan.cui@mail.utoronto.ca>
2021-03-31 14:11:44 -0400

    added some db api

---
+++ models/user.js
@@ -0,0 +1,91 @@
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+	userName: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true,
+		unique: true
+	}, 
+	password: {
+		type: String,
+		required: true
+	},
+
+    isAdmin :{
+        type: Boolean,
+        required: true
+    },
+
+    description: {
+        type: String,
+        default: ""
+    },
+    skills:{
+        type: [String],
+        default: []
+    },
+    experiences:{
+        type: [
+            {
+				company: String,
+				position: String,
+				start: String,
+				end: String,
+            }
+        ],
+        default: []
+    },
+    email:{
+        type: String,
+        default: ""
+    },
+    linkedin:{
+        type: String,
+        default: ""
+    },
+    github:{
+        type: String,
+        default: ""
+    },
+
+    ownedProjectIds: {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+
+    joinedProjectIds: {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+
+    appliedProj: {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+
+    //if it is topped by admin
+    topped: {
+        type: Boolean,
+        default: false
+    },
+
+
+    connections:  {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+
+    //the applicants 
+    pending:  {
+        type: [mongoose.Schema.Types.ObjectId],
+        default: []
+    },
+
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
--- server.js
+++ server.js
@@ -5,7 +5,73 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
 
+const { ObjectID } = require('mongodb')
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+
+
+app.post("/api/newUser", (req, res) => {
+    
+	if (mongoose.connection.readyState != 1){
+		res.status(500).send('Internal server error')
+		return;
+	}
+	
+	const newUser = new User({
+		userName: req.body.userName,
+		password: req.body.password,
+		isAdmin: req.body.isAdmin,
+	});
+
+	newUser.save()
+	.then((result) => {
+		res.send(result)
+	}).catch((error) => {
+		res.status(400).send("Bad Request")
+	})
+})
+
+
+app.patch("/api/updateProfile", async (req, res) => {
+    
+	if (mongoose.connection.readyState != 1){
+		res.status(500).send('Internal server error')
+		return;
+	}
+
+    const profileData = ['description', 'skills', 'experiences', 'email', 'linkedin', 'github'];
+
+    User.findOne({userName: req.body.userName})
+    .then((user) => {
+        if (!user){
+            res.status(404).send('User not found')
+            return;
+        }
+        
+        profileData.forEach((name) => {
+            if (req.body[name]){
+                user[name] = req.body[name];
+            }
+        })
+
+        user.save()
+        .then(result => {
+            res.send(result)
+        }).catch(err => {
+            res.status(400).send("bad request")
+            return;
+        })
+        
+        
+    }).catch((err) => {
+		res.status(500).send("Internal Server error")
+        return;
+    })
+    
+})
 
 
 app.use(express.static(path.join(__dirname, "/client/build")));
